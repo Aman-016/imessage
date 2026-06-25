3405dcd321bcaf989dc691273227d2e5c2f87bdf
Aman-016 <amanmehta.am2004@gmail.com>
2026-06-25 23:06:19 +0530

    Connect DB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,19 @@
 import express from "express";
 import "dotenv/config";
+import { connectDB } from "./lib/db.js";
+import User from "./models/user.model.js";
+
+await connectDB();
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Server is healthy" });
+});
+
 app.listen(PORT, () => {
   console.log("Server is up and running on PORT:", PORT);
+
 });
+
