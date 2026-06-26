4aca3012e501ba93776d0ec7dc83fef0167f0488
Aman-016 <amanmehta.am2004@gmail.com>
2026-06-26 10:34:24 +0530

    Refactor package.json and index.js to include devDependencies and middleware setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,20 @@ import express from "express";
 import "dotenv/config";
 import { connectDB } from "./lib/db.js";
 import User from "./models/user.model.js";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+
 
 await connectDB();
 
 const app = express();
 const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(express.json());
+app.use(cors({origin: FRONTEND_URL, credentials: true}));
+app.use(clerkMiddleware()); 
+
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Server is healthy" });
