101b33c74758a6f006f23a5d63b611315918e5f2
Aman-016 <amanmehta.am2004@gmail.com>
2026-06-27 12:02:41 +0530

    feat: add image and video upload functionality with ImageKit integration
    - Added @imagekit/nodejs dependency for media uploads.
    - Implemented multer middleware for handling file uploads.
    - Created image upload utility functions in imagekit.js.
    - Developed message controller methods for sending and retrieving messages.
    - Established socket.io server for real-time messaging.
    - Introduced authentication middleware to protect routes.
    - Created auth and message routes for user authentication and messaging features.
    - Updated server initialization to include socket.io and cron job for production.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,37 +1,43 @@
 import express from "express";
-import "dotenv/config";
 import cors from "cors";
+
+import "dotenv/config";
+
 import fs from "fs";
 import path from "path";
+
 import { clerkMiddleware } from "@clerk/express";
 
-import { connectDB } from "./lib/db.js";
 import User from "./models/user.model.js";
+import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
+
 import clerkWebhook from "./webhooks/clerk.webhook.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-await connectDB();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
 
-// Clerk webhook (must come before express.json())
-app.use(
-  "/api/webhooks/clerk",
-  express.raw({ type: "application/json" }),
-  clerkWebhook
-);
+// it's important that you don't parse the webhook event data, it should be in the raw format
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
-  res.status(200).json({ message: "Server is healthy" });
+  res.status(200).json({ ok: true });
 });
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// if the public directory exists, serve the static files
+// this is for the production build
 if (fs.existsSync(publicDir)) {
   app.use(express.static(publicDir));
 
@@ -40,6 +46,9 @@ if (fs.existsSync(publicDir)) {
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
+server.listen(PORT, () => {
+  connectDB();
+  console.log("Server is up and running on PORT:", PORT);
+
+  if (process.env.NODE_ENV === "production") job.start();
 });
